7b10d1557e2e0a7174331e240e384db6543dcb66
PrajwalWarokar <prajwalwarokar2002@gmail.com>
2025-02-11 14:56:40 +0530

    start the project

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          StudentManagment
         </a>
       </header>
     </div>
